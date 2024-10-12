eece1ba697750f1ebd46c25d254e17ed059db38c
Paul Maloney <paul@paulmaloney.com>
2024-10-12 12:35:56 -0700

    finished

--- frontend/components/Todos.js
+++ frontend/components/Todos.js
@@ -1,30 +1,39 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const StyledTodo = styled.li`
-  text-decoration: ${pr => pr.$complete ? 'line-through' : 'initial'};
+  text-decoration: ${(pr) => (pr.$complete ? "line-through" : "initial")};
   cursor: pointer;
-`
+`;
 
 export default function Todo({
   todos,
+  toggleTodo,
+  toggleShowCompletedTodos,
+  showCompleted,
 }) {
   return (
     <div id="todos">
       <h3>Todos</h3>
       <ul>
-        {
-          todos
-            .map(todo => (
-              <StyledTodo $complete={todo.complete} key={todo.id}>
-                <span>{todo.label}{todo.complete && ' ✔️'}</span>
+        {todos.map((todo) => (
+              (todo.complete && !showCompleted) ? null :
+              <StyledTodo
+                $complete={todo.complete}
+                key={todo.id}
+                onClick={() => toggleTodo(todo.id)}
+              >
+                  <span>
+                    {todo.label}
+                    {todo.complete && " ✔️"}
+                  </span>
               </StyledTodo>
-            ))
-        }
+            )
+        )}
       </ul>
-      <button>
-        Hide completed todos
+      <button onClick={toggleShowCompletedTodos}>
+        {showCompleted ? "Hide" : "Show"} completed todos
       </button>
     </div>
-  )
+  );
 }
